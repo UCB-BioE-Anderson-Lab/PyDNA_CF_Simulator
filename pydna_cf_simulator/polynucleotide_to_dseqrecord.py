1674b1e2868e74131cc0fe23a65aa01e509ec7f9
J. Christopher Anderson <jcanderson@berkeley.edu>
2023-06-07 07:55:53 -0700

    conversion tests passing
    handling circularity, checks on inputs seqs

--- pydna_cf_simulator/polynucleotide_to_dseqrecord.py
+++ pydna_cf_simulator/polynucleotide_to_dseqrecord.py
@@ -1,11 +1,27 @@
+import re
 from pydna.dseq import Dseq
 from pydna.dseqrecord import Dseqrecord
 from pydna.utils import rc
 
 
 def polynucleotide_to_dseqrecord(poly):
+    # Check the sequence data
+    if not re.match(r'^[ATCGNRKYSWBVHDM]+$', poly.sequence):
+        raise ValueError("Invalid characters in sequence. Sequence must only contain the characters ATCGNRKYSWBVHDM.")
+
+    if poly.ext5 and not re.match(r'^(-)?[ATCG]+$', poly.ext5):
+        raise ValueError("Invalid characters in ext5. ext5 must only contain the characters ATCG and optionally start with a '-'.")
+
+    if poly.ext3 and not re.match(r'^(-)?[ATCG]+$', poly.ext3):
+        raise ValueError("Invalid characters in ext3. ext3 must only contain the characters ATCG and optionally start with a '-'.")
+
+    # Check that circular sequences have no sticky ends
+    if poly.is_circular and (poly.ext5 or poly.ext3):
+        raise Exception("Circular Polynucleotide cannot have overhangs")
+
     # Grab the 'sequences' portion of the Polynucleotide
     coding_strand = poly.sequence
+    
     # Calculate the reverse complement
     complementary_strand = rc(coding_strand)
 
@@ -30,4 +46,9 @@ def polynucleotide_to_dseqrecord(poly):
 
     # Create Dseqrecord
     sequence = Dseq(coding_strand, complementary_strand, overhang)
+
+    # Loop it if it's circular
+    if poly.is_circular:
+        sequence = sequence.looped()
+
     return Dseqrecord(sequence)
--- tests/test_conversion.py
+++ tests/test_conversion.py
@@ -3,13 +3,21 @@ from pydna_cf_simulator.polynucleotide import Polynucleotide
 from pydna_cf_simulator.polynucleotide_to_dseqrecord import polynucleotide_to_dseqrecord
 from pydna_cf_simulator.dseqrecord_to_polynucleotide import dseqrecord_to_polynucleotide
 
-# Define test examples
 test_examples = [
-    # description, sequence, ext_5, ext_3, is_circular
     ('Blunt both ends', 'ATGCGCTGAC', '', '', False),
     ('Long top strand', 'ATGCGCTGAC', 'CGAA', '-TC', False),
     ('Long bottom strand', 'ATGCGCTGAC', '-CGAA', 'TC', False),
-    # Add more examples as needed
+    ('Lefty blunt, righty 5\' extension', 'ATGCGCTGAC', '', '-TC', False),
+    ('Lefty blunt, righty 3\' extension', 'ATGCGCTGAC', '', 'TC', False),
+    ('Righty blunt, lefty 5\' extension', 'ATGCGCTGAC', 'CGAA', '', False),
+    ('Righty blunt, lefty 3\' extension', 'ATGCGCTGAC', '-CGAA', '', False),
+    ('Circular sequence with no overhangs', 'ATGCGCTGAC', '', '', True)
+]
+
+exception_test_examples = [
+    ('Circular sequence with overhangs (ext5)', 'ATGCGCTGAC', 'CGAA', '', True),
+    ('Circular sequence with overhangs (ext3)', 'ATGCGCTGAC', '', 'TC', True),
+    ('Invalid characters in sequence', 'ATGCGCTGACX', '', '', False)
 ]
 
 @pytest.mark.parametrize('description,sequence,ext_5,ext_3,is_circular', test_examples)
@@ -28,6 +36,11 @@ def test_round_trip_conversion(description, sequence, ext_5, ext_3, is_circular)
     # Convert back to Polynucleotide
     converted_poly = dseqrecord_to_polynucleotide(dseqrecord, 'phosphate', 'phosphate')
     print(f'Converted Polynucleotide: {converted_poly}')
-    
+
     # Assert that the converted Polynucleotide is equal to the original
     assert converted_poly == original_poly
+@pytest.mark.parametrize('description,sequence,ext_5,ext_3,is_circular', exception_test_examples)
+def test_exception_cases(description, sequence, ext_5, ext_3, is_circular):
+    with pytest.raises(Exception):
+        original_poly = Polynucleotide(sequence, ext_5, ext_3, True, is_circular, 'phosphate', 'phosphate')
+        dseqrecord = polynucleotide_to_dseqrecord(original_poly)
